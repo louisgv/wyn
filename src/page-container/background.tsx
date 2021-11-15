fb9bb6cf559da22d7fbbce5511d135c909693534
L <6723574+louisgv@users.noreply.github.com>
2021-11-14 21:18:50 -0800

    Delete src directory

--- src/page-container/background.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/native"
-import { Appearance } from "react-native"
-
-export const Background = styled.View`
-  background-color: ${Appearance.getColorScheme() === "dark" ? "#000" : "#fff"};
-`
