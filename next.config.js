b6e0550704962bf09c1911be8a9badeace20040a
Karlos Santos <6723574+louisgv@users.noreply.github.com>
2021-11-15 01:37:10 -0400

    Ready to test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withPWA = require("next-pwa")
+const runtimeCaching = require("next-pwa/cache")
 
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
-    dest: "public"
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/]
   }
 })
